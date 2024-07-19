e77253eb4b9be101675c28f9e31c575eaf2254b6
amitabh-anandpd <168374056+amitabh-anandpd@users.noreply.github.com>
2024-07-19 18:45:06 +0530

    Add files via upload

---
+++ IMU_Classifier_2/IMU_Classifier_2.ino
@@ -0,0 +1,134 @@
+#include <Arduino_LSM9DS1.h>
+#include <TensorFlowLite.h>
+#include <tensorflow/lite/micro/all_ops_resolver.h>
+#include <tensorflow/lite/micro/micro_error_reporter.h>
+#include <tensorflow/lite/micro/micro_interpreter.h>
+#include <tensorflow/lite/schema/schema_generated.h>
+#include <tensorflow/lite/version.h>
+
+#include "model.h"
+
+const float accelerationThreshold = 2.5; // threshold of significant in G's
+const int numSamples = 120;
+
+int samplesRead = numSamples;
+
+// global variables used for TensorFlow Lite (Micro)
+tflite::MicroErrorReporter tflErrorReporter;
+
+// pull in all the TFLM ops, you can remove this line and
+// only pull in the TFLM ops you need, if would like to reduce
+// the compiled size of the sketch.
+tflite::AllOpsResolver tflOpsResolver;
+
+const tflite::Model* tflModel = nullptr;
+tflite::MicroInterpreter* tflInterpreter = nullptr;
+TfLiteTensor* tflInputTensor = nullptr;
+TfLiteTensor* tflOutputTensor = nullptr;
+
+// Create a static memory buffer for TFLM, the size may need to
+// be adjusted based on the model you are using
+constexpr int tensorArenaSize = 8 * 1024;
+byte tensorArena[tensorArenaSize] __attribute__((aligned(16)));
+
+// array to map gesture index to a name
+const char* GESTURES[] = {
+    "forehand_chop",
+    "backhand_chop",
+    "forehand_counter",
+    "backhand_ctr"
+};
+const int NUM_GESTURES = sizeof(GESTURES)/sizeof(GESTURES[0]);
+
+void setup() {
+  // initialize the IMU
+  if (!IMU.begin()) {
+    // Failed to initialize IMU, turn on the LED to indicate error
+    digitalWrite(LED_BUILTIN, HIGH);
+    while (1);
+  }
+  // get the TFL representation of the model byte array
+  tflModel = tflite::GetModel(model);
+  if (tflModel->version() != TFLITE_SCHEMA_VERSION) {
+    // Model schema mismatch, turn on the LED to indicate error
+    digitalWrite(LED_BUILTIN, HIGH);
+    while (1);
+  }
+
+  // Create an interpreter to run the model
+  tflInterpreter = new tflite::MicroInterpreter(tflModel, tflOpsResolver, tensorArena, tensorArenaSize, &tflErrorReporter);
+
+  // Allocate memory for the model's input and output tensors
+  tflInterpreter->AllocateTensors();
+
+  // Get pointers for the model's input and output tensors
+  tflInputTensor = tflInterpreter->input(0);
+  tflOutputTensor = tflInterpreter->output(0);
+  // Set the LED pin as an output
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  float aX, aY, aZ, gX, gY, gZ;
+
+  // wait for significant motion
+  while (samplesRead == numSamples) {
+    if (IMU.accelerationAvailable()) {
+      // read the acceleration data
+      IMU.readAcceleration(aX, aY, aZ);
+
+      // sum up the absolutes
+      float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
+
+      // check if it's above the threshold
+      if (aSum >= accelerationThreshold) {
+        // reset the sample read count
+        samplesRead = 0;
+        break;
+      }
+    }
+  }
+
+  // check if all the required samples have been read since
+  // the last time significant motion was detected
+  while (samplesRead < numSamples) {
+    // check if new acceleration AND gyroscope data is available
+    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+      // read the acceleration and gyroscope data
+      IMU.readAcceleration(aX, aY, aZ);
+      IMU.readGyroscope(gX, gY, gZ);
+
+      // normalize the IMU data between 0 to 1 and store in the model's
+      // input tensor
+      tflInputTensor->data.f[samplesRead * 6 + 0] = (aX + 4.0) / 8.0;
+      tflInputTensor->data.f[samplesRead * 6 + 1] = (aY + 4.0) / 8.0;
+      tflInputTensor->data.f[samplesRead * 6 + 2] = (aZ + 4.0) / 8.0;
+      tflInputTensor->data.f[samplesRead * 6 + 3] = (gX + 2000.0) / 4000.0;
+      tflInputTensor->data.f[samplesRead * 6 + 4] = (gY + 2000.0) / 4000.0;
+      tflInputTensor->data.f[samplesRead * 6 + 5] = (gZ + 2000.0) / 4000.0;
+
+      samplesRead++;
+
+      if (samplesRead == numSamples) {
+        // Run inferencing
+        TfLiteStatus invokeStatus = tflInterpreter->Invoke();
+        if (invokeStatus != kTfLiteOk) {
+          // Invoke failed, turn on the LED to indicate error
+          digitalWrite(LED_BUILTIN, HIGH);
+          while (1);
+        }
+
+        // Loop through the output tensor values from the model
+        for (int i = 0; i < NUM_GESTURES; i++) {
+          if (tflOutputTensor->data.f[i] > 0.98) {
+            digitalWrite(LED_BUILTIN, HIGH);
+            break;
+          }
+          else{
+            digitalWrite(LED_BUILTIN, LOW);
+          }
+        }
+      }
+    }
+  }
+}
